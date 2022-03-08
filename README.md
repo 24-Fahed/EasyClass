b2535f26322c53302f4a99c737d9eb72e8af042c
24fahed <dgntxhydy@icloud.com>
2022-03-09 01:48:21 +0800

    EasyClass更新
    针对学习通答题页面的更改进行了相应改动，修改了一些bug

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # EasyClass
-超星刷课程序
+超星刷课软件
+版本：0.0.2
+作者：Hugo
+
+声明：
+	本品是一款模拟人对网页进行操作的软件，不会加速视频播放等破坏网页播放规则或者对安全造成影响的行为。
+	本程序仅限于学习交流，禁止一切商务使用。
+
+版本更新：
+	针对超星学习通网页变动更新操作方式
+	修复了会重复开始同一个视频的bug
+	修复了课程选择会跳转到其他课程的bug
+
+代码开发说明：
+	本程序使用selenium模块进行开发，请安装python解释器并下载selenium。
+    主程序中包含一个EasyClass类，可以作为单独库导入（记得复制一份然后改名）
+
+使用说明：
+	启动程序：在main文件夹下运行main.exe。
+	本产品支持最新版本火狐浏览器，如果您的电脑上没有火狐浏览器，请安装。
+	产品驱动下载地址：https://github.com/mozilla/geckodriver/releases，请选择最新版下载。
+	在登录之前请先登录您的学习通，复制第一节课的网址。
+	该程序针对老版本超星网页设计，使用时请按照示例将网址删减到规定样式。
+		当前网址：https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245&mooc2=1&cpi=157710188&openc=8983c8940b12e650265152adbd0aeb6c
+		删除的部分：&mooc2=1&cpi=157710188&openc=8983c8940b12e650265152adbd0aeb6c（即将&mooc2=后面的数据全部删除）
+		老版本网址：https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245
+	本程序需要您输入用户名和密码，本人承诺该程序中不含有盗取您账号和密码的行为。
+    在输入账号和密码之后，Firefox浏览器会自动弹出，请不要担心。如果您是第一次使用firefox浏览器，可能会导致程序启动失败，届时请关闭程序重启即可
+    因为网速问题，可能会导致视频时长获取失败，请保证网络通畅。
+
+火狐浏览器驱动下载地址：
+	https://github.com/mozilla/geckodriver/releases
--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from time import sleep
 from re import findall
 import os
 
+
+# https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=443991288&courseId=223543911&clazzid=52846586&enc=a6610ceda146c4bba791421adedaa3d6
+
 class EasyClass(object):
     def __init__(self, classurl, userID, passwd):
         # 创建浏览器驱动
         self.drive = webdriver.Firefox()
         # 创建网址组装格式
-        self.demo = 'https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId={}&courseId={}&clazzid={}&enc={}'.format('', '', '', self.createwebdemo(classurl))
+        # self.demo = 'https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId={}&courseId={}&clazzid={}&enc={}'.format('', '', '', self.createwebdemo(classurl))
+        self.classurl = classurl
         self.userID = userID
         self.passwd = passwd
-        self.classleft = 0
         self.allclass = 0
         self.class_xpath_list = []
         self.flage = True
-
+        # 是否可以开始播放视频，如果不能为False，可以为True
+        self.pagedone = False
+        self.classnum = 0
+        # 是否允许切换视频
+        self.switchclass = False
 
     def createwebdemo(self, classurl):
         # url = 'https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245'
@@ -24,19 +31,19 @@ class EasyClass(object):
         # print(ens)
         return ens
 
-    def log_in(self, url):
+    def log_in(self):
         '''
         使用模块登陆学习通
         :param drive:浏览器驱动
         :param url:登陆的网址
         :return:操作后的（已登陆）驱动
         '''
-        self.drive.get(url)
+        self.drive.get(self.classurl)
         sleep(1)
         content = self.drive.find_element_by_id("phone")
         password = self.drive.find_element_by_id("pwd")
         button = self.drive.find_element_by_id("loginBtn")
-        
+
         content.send_keys(self.userID)
         password.send_keys(self.passwd)
         input("如果有验证码，请先进行人工识别，输入完成后，请回命令行按回车")
@@ -54,6 +61,7 @@ class EasyClass(object):
         js = 'document.getElementsByClassName("{}")[0].click()'.format(tag)
         print(js)
         self.drive.execute_script(js)
+        return 0
 
     def start_video(self):
         '''
@@ -61,6 +69,7 @@ class EasyClass(object):
         :return: NONE
         '''
         # 切换frame
+        self.drive.execute_script("window.scrollBy(0,1000)")
         self.drive.switch_to.frame('iframe')
 
         self.drive.switch_to.frame(0)
@@ -78,23 +87,31 @@ class EasyClass(object):
         '''
         sleep(1)
         # 寻找提交按钮
-        sub_butt = qustion.find_element_by_class_name("ans-videoquiz-submit")
+        sub_butt = self.drive.find_element_by_id("videoquiz-submit")
         # 点击提交按钮
         sub_butt.click()
         return
 
     def check_cycle(self):
-        '''<selenium.webdriver.firefox.webelement.FirefoxWebElement (session="758bdc5d-314a-43c1-974c-3b528182ff47", element="f81fa45c-5ec1-45e8-9da6-30c65727ad6c")>'''
-        '''<selenium.webdriver.firefox.webelement.FirefoxWebElement (session="758bdc5d-314a-43c1-974c-3b528182ff47", element="3e17dffa-5afe-4c32-b6a1-9f76325828be")>'''
-        '''/html/body/div[4]/div/div[1]/span/div/div/ul/li[1]/label/input'''
+        '''/html/body/div[4]/div/div[1]/span/div/div/ul//label/input'''
         '''
         该循环用来检测视频中是否出现答题环节
         :return:NONE
         '''
-        qustion = self.drive.find_element_by_class_name("x-component")
-        quiz = qustion.find_element_by_class_name("ans-videoquiz-title")
+        # qustion = self.drive.find_element_by_class_name("tkItem_title")
+        # quiz = qustion.find_element_by_class_name("ans-videoquiz-title")
+        # print(quiz.text)
+        # opt = qustion.find_elements_by_xpath("/html/body/div[4]/div/div[1]/span/div/div/ul/li/label/input")
+        # 上面为老代码
+        # 超星更新，将答题页面修改为一个iframe
+
+        qustion = self.drive.find_element_by_class_name("tkItem")
+        quiz = qustion.find_element_by_class_name("tkItem_title")
         print(quiz.text)
-        opt = qustion.find_elements_by_xpath("/html/body/div[4]/div/div[1]/span/div/div/ul/li/label/input")
+        # opt = qustion.find_elements_by_xpath("/html/body/div/div[4]/div/div[1]/span/div/div/div/div[2]/div/ul/li[1]/label/span/input")
+        opt = qustion.find_elements_by_xpath(
+            "/html/body/div/div[4]/div/div[1]/span/div/div/div/div[2]/div/ul//label/span/input")
+
         print(opt)
         for opt_t in opt:
             value = opt_t.get_attribute("value")
@@ -103,11 +120,15 @@ class EasyClass(object):
                 self.submit(qustion)
 
     def get_time(self):
-        sleep(20)
+        sleep(10)
         # drive.find_elements_by_xpath()
-        long = self.drive.find_elements_by_xpath('/html/body/div[4]/div/div[5]/div[4]/span[2]')
+        # /html/body/div/div[4]/div/div[5]/div[4]/span[2]
+        # long = self.drive.find_elements_by_xpath('/html/body/div[4]/div/div[5]/div[4]/span[2]')
+        long = self.drive.find_element_by_xpath("/html/body/div/div[4]/div/div[5]/div[4]/span[2]")
+        print(long.get_attribute('outerHTML'))
+        # selenium更新，指定一个标签的时候会直接返回一个对象，不会返回列表了
 
-        strlong = long[0].get_attribute("textContent")
+        strlong = long.get_attribute("textContent")
         min = int(strlong[0: strlong.index(":")])
         sec = int(strlong[strlong.index(":") + 1:])
         print("时常：", min, ':', sec)
@@ -121,6 +142,7 @@ class EasyClass(object):
         :return:
         '''
         t = int(t / 5)
+
         for _ in range(t):
             # process2 = multiprocessing.Process(target=check_cycle)
             # process2.start()
@@ -159,53 +181,49 @@ class EasyClass(object):
             sleep(5)
             self.get_class_xpath()
 
-
     def next_video(self):
         '''
-        播放下一条视频
+        切换到指指定视频
         :param class_xpath_list:
         :param num:
         :return:
         '''
-        self.classleft -= 1
-        if self.classleft == 0:
+        # 如果课程已经刷完
+        if self.classnum == self.allclass:
             self.flage = False
+            return
+
         # 切换回主界面
         self.drive.switch_to.default_content()
-        #print(self.classleft)
-        #print(self.allclass - self.classleft)
-        #print(self.class_xpath_list)
-        xpath_next = self.class_xpath_list[self.allclass - self.classleft] # 出错
-        # print(xpath_next)
-        # print(xpath_next)
+        # 找到指定课程xpath
+        xpath_next = self.class_xpath_list[self.classnum - 1]
+
         b = self.drive.find_elements_by_xpath(xpath_next)
         # print(b)
         b[0].click()
         print("切换到课程：", self.drive.find_elements_by_xpath(xpath_next)[0].get_attribute('textContent'))
-        print("剩余：", self.classleft, "节课")
+        print("剩余：", self.allclass - self.classnum, "节课")
 
     def getClassnum(self):
         '''
         返回剩余课程数量
         :return:
         '''
-        return self.allclass - self.classleft
+        return self.classnum
 
     def setClassnum(self):
         '''
         剩余课程量
         :return:
         '''
-        self.classleft = self.allclass - int(input("请输入课程序号"))
-
-
-
-
+        # self.classleft = self.allclass - int(input("请输入课程序号"))
+        self.classnum = int(input("请输入课程序号"))
 
 
 if __name__ == "__main__":
-    print("欢迎使用刷客软件 | V 0.0.1")
+    print("欢迎使用EasyClass | V 0.0.2")
     print("作者：Hugo")
+    print("正在检测系统环境：")
     print("根据下面的提示完成相应操作，您就可以使用本程序进行刷课了")
     print("====================================================================")
     exam = 'https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245'
@@ -213,34 +231,81 @@ if __name__ == "__main__":
     passwd = input("请输入密码：")
     print("网址示例：", exam)
     url = input("请输入起始网址")
-    #https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245
+    # https://mooc1-1.chaoxing.com/mycourse/studentstudy?chapterId=398481847&courseId=217666369&clazzid=39610013&enc=1812c2df5429653b1f6ed82782730245
     ec = EasyClass(url, userID, passwd)
-    ec.log_in(url)
+    ec.log_in()
     # 获取课程列表
     ec.get_class_xpath()
     ec.setClassnum()
     print("选中课程：", ec.getClassnum())
     # 开始视频播放
+    # while ec.flage != False:
+    #     try:
+    #         sleep(5)
+    #         ec.go_to_videoPage("c2")
+    #         sleep(5)
+    #         ec.start_video()
+    #     except Exception as e:
+    #         print(e)
+    #         ec.go_to_videoPage("c1")
+    #         sleep(3)
+    #         ec.start_video()
+    #     try:
+    #         time = ec.get_time()
+    #         if time == 0:
+    #             print("重新尝试获取时长")
+    #             time = ec.get_time()
+    #         else:
+    #             ec.start_check(time)
+    #             ec.next_video()
+    #     except Exception as ee:
+    #         print(ee)
+
+    # 开始视频播放
+    vidio_tag_list = ['c1', 'c2']
     while ec.flage != False:
+        # 定位到标签并开始视频
         try:
-            sleep(5)
-            ec.go_to_videoPage("c2")
-            sleep(5)
-            ec.start_video()
-        except Exception as e:
-            print(e)
-            ec.go_to_videoPage("c1")
-            sleep(3)
-            ec.start_video()
+            # 切换视频到指定位置
+            if ec.switchclass == False:
+                try:
+                    ec.next_video()
+                    ec.switchclass = True
+                except:
+                    continue
+
+            if ec.pagedone == False:
+                for i in vidio_tag_list:
+                    sleep(5)
+                    try:
+                        ec.go_to_videoPage(i)
+                        sleep(5)
+                        ec.start_video()
+                        sleep(5)
+                        ec.pagedone = True
+                        break
+                    except:
+                        print("尝试切换标签")
+                        continue
+        except:
+            print("视频所在页面标签定位错误")
+            continue
+
+        # 开始视频播放并检测是否进入下一节
         try:
             time = ec.get_time()
             if time == 0:
-                print("重新尝试获取时长")
+                # 尝试重新获取时间
+                print("重新获取时常")
                 time = ec.get_time()
             else:
                 ec.start_check(time)
+                ec.pagedone = False
+                ec.switchclass = False
+                ec.classnum += 1
                 ec.next_video()
-        except Exception as ee:
-            print(ee)
+        except Exception as err:
+            print("未知错误")
+            print(err)
 
     os.system('pause')
